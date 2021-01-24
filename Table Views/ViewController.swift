1f833a25979ae6acde0515c1d17c5be15b0a62c8
alanrmendez <alan.mendez07@gmail.com>
2021-01-23 22:28:08 -0500

    Initial Commit

---
+++ Table Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift.
+//  Table Views
+//
+//  Created by Alan Mendez on 1/23/21.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
